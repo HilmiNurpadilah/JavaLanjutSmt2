fdb31313c759fb7d8f178594a2c58a30af2e7930
HilmiNurpadilah <158576356+HilmiNurpadilah@users.noreply.github.com>
2024-07-26 21:03:34 +0700

    Delete Tugas4 directory

--- Tugas4/Shape.java
+++
@@ -1,9 +0,0 @@
-public abstract class Shape {
-    public abstract double getArea();
-    public abstract double getPerimeter();
-
-    public void display() {
-        System.out.println("Area: " + getArea());
-        System.out.println("Perimeter: " + getPerimeter());
-    }
-}
